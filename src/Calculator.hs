745642714add1079e21953612ed761bcc48c371a
Latiif <llusx@outlook.com>
2018-12-17 13:19:29 +0100

    Add zoom feature

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -12,7 +12,7 @@ canWidth,canHeight :: Num a => a
 canWidth  = 300
 canHeight = 300
 
-origScale = 0.04
+origScale = 0.02
 
 main :: IO ()
 main = startGUI defaultConfig setup
@@ -25,13 +25,22 @@ setup window =
      input   <- mkInput 20 "x"                -- The formula input
      draw    <- mkButton "Draw graph"         -- The draw button
      diff    <- mkButton "Differentiate"      -- The diff button
+
+     --
+     slider  <- mkSlider (50,200) 100 
+     maxZoom <- mkHTML "<b>200%</b>"
+     minZoom <- mkHTML "<b>50%</b>"
+
+     zoomBox <- column [row [pure minZoom, pure slider, pure maxZoom]]
+    
        -- The markup "<i>...</i>" means that the text inside should be rendered
        -- in italics.
 
      -- Add the user interface elements to the page, creating a specific layout
      formula <- row [pure fx,pure input]
      btns    <- row [pure draw, pure diff]
-     getBody window #+ [column [pure canvas,pure formula,pure btns]]
+
+     getBody window #+ [column [pure canvas,pure formula,pure btns, pure zoomBox]]
 
      -- Styling
      getBody window # set style [("backgroundColor","lightblue"),
@@ -40,15 +49,16 @@ setup window =
      pure btns  # set style [("align","center")]
 
      -- Interaction (install event handlers)
-     on UI.click     draw  $ \ _ -> readAndDraw input canvas
-     on valueChange' input $ \ _ -> readAndDraw input canvas
-     on UI.click     diff  $ \ _ -> readAndDiff input canvas
-
-
-readAndDraw :: Element -> Canvas -> UI ()
-readAndDraw input canvas =
+     on UI.click     draw  $ \ _ -> readAndDraw input canvas slider
+     on valueChange' input $ \ _ -> readAndDraw input canvas slider
+     on UI.click     diff  $ \ _ -> readAndDiff input canvas slider
+     on (domEvent "change") slider $ \ _ -> readAndDraw input canvas slider
+          --    textbox     canvas    slider
+readAndDraw :: Element -> Canvas -> Element -> UI ()
+readAndDraw input canvas slider =
   do -- Get the current formula (a String) from the input element
      formula <- get value input
+     scale <- get value slider
      -- Clear the canvas
      clearCanvas canvas
      -- The following code draws the formula text in the canvas and a blue line.
@@ -56,16 +66,17 @@ readAndDraw input canvas =
      set UI.fillStyle (UI.solidColor (UI.RGB 0 0 0)) (pure canvas)
      case readExpr formula of 
                             (Just exp) -> do
-                                              path "blue" (points exp origScale (canHeight,canHeight)) canvas
+                                              path "blue" (points exp (calculateScale scale) (canHeight,canHeight)) canvas
                                               UI.fillText ((showExpr . simplify ) exp) (10,canHeight/2) canvas
 
                             _ -> UI.fillText "WRONG" (10,canHeight/2) canvas
 
 
-readAndDiff :: Element -> Canvas -> UI ()
-readAndDiff input canvas =
+readAndDiff :: Element -> Canvas -> Element -> UI ()
+readAndDiff input canvas slider =
   do -- Get the current formula (a String) from the input element
      formula <- get value input
+     scale <- get value slider
      -- Clear the canvas
      --clearCanvas canvas
      -- The following code draws the formula text in the canvas and a blue line.
@@ -73,7 +84,7 @@ readAndDiff input canvas =
      set UI.fillStyle (UI.solidColor (UI.RGB 0 0 0)) (pure canvas)
      case readExpr formula of 
                             (Just exp) -> do
-                                              path "red" (points (differentiate exp) origScale (canHeight,canHeight)) canvas
+                                              path "red" (points (differentiate exp) (calculateScale scale) (canHeight,canHeight)) canvas
                                               UI.fillText ((showExpr . simplify ) exp) (10,canHeight/2) canvas
 
                             _ -> UI.fillText "WRONG" (10,canHeight/2) canvas                            
@@ -93,5 +104,7 @@ points exp scale (width, height) = [(pixel, cartesianToPixel (eval exp (pixelToC
       cartesianToPixel :: Double -> Double
       cartesianToPixel y = (y - (fromIntegral height * scale) / 2) / ((-1) * scale)
 
-
+calculateScale :: String -> Double
+calculateScale str = percentage*origScale / (100)
+              where percentage = read (str):: Double
 
